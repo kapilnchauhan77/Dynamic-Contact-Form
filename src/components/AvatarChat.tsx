e5790fb100013d4d4238b67b7d4e62a1b7bcef2e
kapilnchauhan77 <kapilnchauhan77@gmail.com>
2024-02-26 14:22:03 +0530

    Small UI changes

--- src/components/AvatarChat.tsx
+++ src/components/AvatarChat.tsx
@@ -248,7 +248,7 @@ function AvatarChat() {
                             :
                             (
                             <div>
-                                <div className="mt-10">
+                                <div className="mt-10 pb-8">
                                     <button
                                         className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                         onClick={handleGenInfo}
--- src/components/utils/postToAPI.ts
+++ src/components/utils/postToAPI.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://survey.xpbrand.ai/api/";
-// const BASE_URL = "http://127.0.0.1:5000/api/";
+// const BASE_URL = "https://survey.xpbrand.ai/api/";
+const BASE_URL = "http://127.0.0.1:5000/api/";
 export const postToAPI = async (url:string, params: any) => {
     const apiClient = axios.create({
       baseURL: BASE_URL, 
